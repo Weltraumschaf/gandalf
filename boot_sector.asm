27438dbc6e04af060c928476dac590b29341bb8c
Sven Strittmatter <ich@weltraumschaf.de>
2014-02-04 23:09:32 +0100

    Use the stack.

--- boot_sector.asm
+++ boot_sector.asm
@@ -1,22 +1,32 @@
 ; 
-; Asimple boot sector
+; A simple boot sector program that demonstrates addressing.
 ;
 
-mov ah, 0x0e
+[org 0x7c00]
 
-mov al, 'H'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'o'
-int 0x10
+  mov ah, 0x0e    ; int 10/ah = 0eh -> scrolling teletype BIOS routine
+  
+  mov bp, 0x8000  ; Set the base of the stack a little above where BIOS
+  mov sp, bp      ; loads our boot sector - so it won’t overwrite us.
+  
+  push 'A'
+  push 'B'
+  push 'C'
+  
+  pop bx
+  mov al, bl
+  int 0x10
+  
+  pop bx
+  mov al, bl
+  int 0x10
+  
+  mov al, [0x7ffe]
+  int 0x10
+  
+  jmp $
 
-jmp $
+; Padding and magic BIOS number.
 
-times 510 - ( $ - $$ ) db 0
-
-dw 0xaa55
+  times 510 - ( $ - $$ ) db 0
+  dw 0xaa55
