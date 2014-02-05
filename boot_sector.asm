ead79620eb5ff3a2d42ef1fa9daa2bdf2b8d42ff
Sven Strittmatter <ich@weltraumschaf.de>
2014-02-05 22:26:56 +0100

    Try to load disk.

--- boot_sector.asm
+++ boot_sector.asm
@@ -4,12 +4,26 @@
 
 [org 0x7c00] ; Tell the assembler where this code will be loaded.
 
-  mov bx, HELLO_MSG ; Use BX as a parameter to our function, so
-  call print_string ; we can specify the address of a string.
+  mov bx, HELLO_MSG       ; Use BX as a parameter to our function, so
+  call print_string       ; we can specify the address of a string.
 
-;  mov dx, 0x1fb6
-;  call print_hex
-
+  mov [BOOT_DRIVE], dl    ; BIOS stores our boot drive in DL, so it's 
+                          ; best to remember this forlater.
+  
+  mov bp, 0x8000          ; Here we set our stack safely out of the
+  mov sp, bp              ; way, at 0x8000.
+  
+  mov bx, 0x9000          ; Load 2 sectors to 0x0000(ES):0x9000(BX)
+  mov dh, 2               ; from the boot disk.
+  mov dl, [BOOT_DRIVE]
+  call disk_load
+  
+  mov dx, [0x9000]        ; Print out the first loaded word, which
+  call print_hex          ; we expect to be 0xdada, stored at adress 0x9000.
+  
+  mov dx, [0x9000 + 512]  ; Also, print the first word from the 
+  call print_hex          ; 2nd loaded sector: should be 0xface
+  
   mov bx, GOODBYE_MSG
   call print_string
 
@@ -17,15 +31,18 @@
 
 %include "print_string.asm"
 %include "print_hex.asm"
+%include "disk_load.asm"
 
 ; Data
 
-HELLO_MSG:
-  db 'Hello, World!', 0
-GOODBYE_MSG:
-  db 'Goodbye!', 0
+HELLO_MSG:    db 'Hello, World!', 0
+GOODBYE_MSG:  db 'Goodbye!', 0
+BOOT_DRIVE:   db 0
   
 ; Padding and magic BIOS number.
 
   times 510 - ( $ - $$ ) db 0
   dw 0xaa55
+
+times 256 dw 0xdada
+times 256 dw 0xface
