40460871069425ff49727329835985aa4e2fe0e6
Sven Strittmatter <ich@weltraumschaf.de>
2014-02-09 14:17:05 +0100

    Add CRLF to data definition.

--- src/asm/16bit/print_string.asm
+++ src/asm/16bit/print_string.asm
@@ -1,25 +1,20 @@
 [bits 16]
 
-print_string:
-  pusha
-  mov ah, 0x0e          ; ISR for screen
+section .text
+  print_string:
+    pusha
+    mov ah, 0x0e          ; ISR for screen
 
-print_string_loop:
-  mov al, [bx]          ; Store the char at BX in AL
+  .loop:
+    mov al, [bx]          ; Store the char at BX in AL
   
-  cmp al, 0             ; if (al == 0), at end of string, so
-  je print_string_done  ; jump to done
+    cmp al, 0             ; if (al == 0), at end of string, so
+    je .done              ; jump to done
   
-  int 0x10
-  add bx, 1
-  jmp print_string_loop
+    int 0x10
+    add bx, 1
+    jmp .loop
 
-print_string_done:
-  mov al, 0x0a ; Print LF
-  int 0x10
-  
-  mov al, 0x0d ; Print CR
-  int 0x10
-  
-  popa
-  ret
+  .done:  
+    popa
+    ret
