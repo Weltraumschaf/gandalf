da45b665c437056264b898f1dedb5b8f816a7afa
Sven Strittmatter <ich@weltraumschaf.de>
2014-02-14 23:12:58 +0100

    Add license info for klibc.

--- src/c/libc/strcpy.c
+++ src/c/libc/strcpy.c
@@ -1,3 +1,10 @@
+/**
+ * This source is from https://en.wikipedia.org/wiki/Klibc which is provided 
+ * under the https://gnu.org/licenses/gpl.html or http://www.linfo.org/bsdlicense.html
+ * thanks to the programmers saving me lot of time so i can concetrate on 
+ * learning to make a kernel.
+ */
+
 /*
  * strcpy.c
  *
