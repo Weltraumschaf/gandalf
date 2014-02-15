7ce4e7a4677ae1d5839b6f7b648bbe252e81cf23
Sven Strittmatter <ich@weltraumschaf.de>
2014-02-15 14:59:48 +0100

    Add more libc implementation examples.

---
+++ lib/libc/README.md
@@ -0,0 +1,6 @@
+# libc
+
+Some [libc][libc] implementations to [compared][compare].
+
+[libc]: https://en.wikipedia.org/wiki/C_standard_library
+[compare]: http://www.etalabs.net/compare_libcs.html
---
+++ lib/libc/musl-0.9.15/src/dirent/readdir.c
@@ -0,0 +1,23 @@
+#include <dirent.h>
+#include "__dirent.h"
+#include "libc.h"
+
+int __getdents(int, struct dirent *, size_t);
+
+struct dirent *readdir(DIR *dir)
+{
+	struct dirent *de;
+	
+	if (dir->buf_pos >= dir->buf_end) {
+		int len = __getdents(dir->fd, (void *)dir->buf, sizeof dir->buf);
+		if (len <= 0) return 0;
+		dir->buf_end = len;
+		dir->buf_pos = 0;
+	}
+	de = (void *)(dir->buf + dir->buf_pos);
+	dir->buf_pos += de->d_reclen;
+	dir->tell = de->d_off;
+	return de;
+}
+
+LFS64(readdir);
