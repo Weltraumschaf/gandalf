ff8343a67c20deb550e09aedae7d967a86d7eb2a
Sven Strittmatter <sven.strittmatter@iteratec.de>
2016-02-01 19:16:17 +0100

    Document the extended ASM.

--- src/c/kernel/arch/i386/low_level.c
+++ src/c/kernel/arch/i386/low_level.c
@@ -2,14 +2,25 @@
 
 // From http://wiki.osdev.org/Inline_Assembly/Examples
 
+/*
+ * Extended ASM:
+ *
+ * asm( assembler template
+ *    : output operands
+ *    : input operands
+ *    : list of clobbered registers 
+ *    );
+ */
+ 
 uint32_t farpeekl(uint16_t sel, void *off) {
     uint32_t ret;
     asm("push %%fs\n\t"
         "mov  %1, %%fs\n\t"
         "mov  %%fs:(%2), %0\n\t"
         "pop  %%fs"
-        : "=r"(ret)
-        : "g"(sel), "r"(off));
+        : "=r"(ret) /* output operands */
+        : "g"(sel), "r"(off) /* input operands */
+        );
     return ret;
 }
 
@@ -18,13 +29,17 @@ void farpokeb(uint16_t sel, void *off, uint8_t v) {
         "mov  %0, %%fs\n\t"
         "movb %2, %%fs:(%1)\n\t"
         "pop %%fs"
-        :
-        : "g"(sel), "r"(off), "r"(v));
+        : /* no output operands */
+        : "g"(sel), "r"(off), "r"(v) /* input operands */
+        );
     /* TODO: Should "memory" be in the clobber list here? */
 }
 
 void port_byte_out(uint16_t port, uint8_t val) {
-    asm volatile("outb %0, %1" : : "a"(val), "Nd"(port));
+    asm volatile("outb %0, %1" 
+        : /* no output operands */
+        : "a"(val), "Nd"(port) /* input operands */
+        );
     /* TODO: Is it wrong to use 'N' for the port? It's not a 8-bit constant. */
     /* TODO: Should %1 be %w1? */
     /* TODO: Is there any reason to force the use of eax and edx? */
@@ -32,7 +47,10 @@ void port_byte_out(uint16_t port, uint8_t val) {
 
 uint8_t port_byte_in(uint16_t port) {
     uint8_t ret;
-    asm volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    asm volatile("inb %1, %0" 
+        : "=a"(ret) /* output operands */ 
+        : "Nd"(port) /* input operands */
+        );
     /* TODO: Is it wrong to use 'N' for the port? It's not a 8-bit constant. */
     /* TODO: Should %1 be %w1? */
     /* TODO: Is there any reason to force the use of eax and edx? */
@@ -42,7 +60,10 @@ uint8_t port_byte_in(uint16_t port) {
 void io_wait(void) {
     /* Port 0x80 is used for 'checkpoints' during POST. */
     /* The Linux kernel seems to think it is free for use :-/ */
-    asm volatile("outb %%al, $0x80" : : "a"(0));
+    asm volatile("outb %%al, $0x80" 
+        : /* no output operands */ 
+        : "a"(0) /* input operands */
+        );
     /* TODO: Is there any reason why al is forced? */
 }
 
@@ -50,7 +71,8 @@ bool are_interrupts_enabled() {
     unsigned long flags;
     asm volatile("pushf\n\t"
                  "pop %0"
-                 : "=g"(flags));
+                 : "=g"(flags) /* output operands */
+                 );
     return flags & (1 << 9);
     return false;
 }
@@ -63,37 +85,63 @@ void lidt(void *base, uint16_t size) {
 
     IDTR.length = size;
     IDTR.base = (uint32_t)base;
-    asm volatile("lidt (%0)" : : "p"(&IDTR));
+    
+    asm volatile("lidt (%0)" 
+        : /* no output operands */ 
+        : "p"(&IDTR) /* input operands */
+        );
 }
 
 void cpuid(int code, uint32_t *a, uint32_t *d) {
-    asm volatile("cpuid" : "=a"(*a), "=d"(*d) : "0"(code) : "ebx", "ecx");
+    asm volatile("cpuid" 
+        : "=a"(*a), "=d"(*d) /* output operands */
+        : "0"(code) /* input operands */
+        : "ebx", "ecx" /* clobbered registers */
+        );
 }
 
 void rdtsc(uint32_t *upper, uint32_t *lower) {
     /* TODO: It may be better to return a struct instead of using pointers. */
-    asm volatile("rdtsc" : "=a"(*lower), "=d"(*upper));
+    asm volatile("rdtsc" 
+        : "=a"(*lower), "=d"(*upper) /* output operands */
+        );
 }
 
 unsigned long read_cr0(void) {
     unsigned long val;
-    asm volatile("mov %%cr0, %0" : "=r"(val));
+    
+    asm volatile("mov %%cr0, %0" 
+        : "=r"(val) /* output operands */
+        );
+    
     return val;
 }
 
 void invlpg(void *m) {
     /* Clobber memory to avoid optimizer re-ordering access before invlpg,
        which may cause nasty bugs. */
-    asm volatile("invlpg %0" : : "m"(m) : "memory" );
+    asm volatile("invlpg %0" 
+        : /* no output operands */
+        : "m"(m) /* input operands */
+        : "memory" /* clobbered registers */
+        );
 }
 
 void wrmsr(uint32_t msr_id, uint64_t msr_value) {
-    asm volatile("wrmsr" : : "c"(msr_id), "A"(msr_value));
+    asm volatile("wrmsr" 
+        : /* no output operands */
+        : "c"(msr_id), "A"(msr_value) /* input operands */
+        );
 }
 
 uint64_t rdmsr(uint32_t msr_id) {
     uint64_t msr_value;
-    asm volatile("rdmsr" : "=A"(msr_value) : "c"(msr_id));
+    
+    asm volatile("rdmsr" 
+        : "=A"(msr_value) /* output operands */
+        : "c"(msr_id) /* input operands */
+        );
+    
     return msr_value;
 }
 
