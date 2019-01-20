31dba6c4d8dfbae8623ff85d2737f0627fa2b2d4
Sven Strittmatter <sven.strittmatter@iteratec.de>
2019-01-20 19:47:02 +0100

    Write some unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+#![cfg_attr(not(test), no_std)] // don't link the Rust standard library
+#![cfg_attr(not(test), no_main)] // disable all Rust-level entry points
+#![cfg_attr(test, allow(unused_imports))]
 
 use core::panic::PanicInfo;
 
 mod vga_buffer;
 
 /// This function is called on panic.
+#[cfg(not(test))] // only compile when the test flag is not set
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
+#[cfg(not(test))] // only compile when the test flag is not set
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
